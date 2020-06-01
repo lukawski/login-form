30d0f468571da52ccf7aae77f79fb8d30d5c70dc
Kacper Łukawski <kacper.lukawski@lastminute.com>
2020-06-01 22:11:58 +0200

    feat: form validation

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
-type Inputs = {
-  example: string;
-  exampleRequired: string;
+type UserLoginData = {
+  email: string;
+  password: string;
 };
 type LoginFormProps = {
-  onSubmit: (data: any) => void;
+  onSubmit: (data: UserLoginData) => void;
 };
 
+const validationSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required().min(5),
+});
+
 function LoginForm({ onSubmit }: LoginFormProps) {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const { register, handleSubmit, errors } = useForm<UserLoginData>({
+    validationSchema,
+  });
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <input name="email" type="email" ref={register({ required: true })} />
+        <input name="email" type="email" ref={register()} />
+        <p>{errors.email?.message}</p>
       </div>
       <div>
-        <input
-          name="password"
-          type="password"
-          ref={register({ required: true })}
-        />
+        <input name="password" type="password" ref={register()} />
+        <p>{errors.password?.message}</p>
       </div>
       <input type="submit" />
     </form>
